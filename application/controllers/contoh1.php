eab5fb9bef32035ac8431a91ad128210c0cd619f
ariskha <dheaariskha@gmail.com>
2023-09-30 22:46:42 +0700

    menambahkan application/controllers/latihan1.php,application/models/model_latihan1.php,application/views/view-latihan.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya Dhea Ariskha
+        saya tinggal di Tambun
+        Hobi saya badminton";
+    }
+}
