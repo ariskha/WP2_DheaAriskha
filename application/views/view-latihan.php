d5a81635bf4bc6299e98e310a2db0a49e3ce3250
ariskha <dheaariskha@gmail.com>
2023-10-03 15:33:36 +0700

    application/controllers/web.php, application/views/v_footer.php, application/views/v_header.php, application/views/v_index.php, assets/css/stylebuku.css

--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -1,16 +1,15 @@
 <html>
 
 <head>
-    <title>Latihan1</title>
-</head>
+ <title>Latihan 1</title>
 
+ </head>
 <body>
-    Halo kawan.. Yuk kita belajar web programming..!!!<br>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
     Nilai 1 = <?= $nilai1; ?>
     Nilai 2 = <?= $nilai2; ?>
     ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
-$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
-
+                                                                $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
 
 </html>
